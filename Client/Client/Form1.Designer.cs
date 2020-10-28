a47e488abba030918e8411a9841601906e20dae8
alexmoyasanchez <alex.moya.sanchez@gmail.com>
2020-10-28 12:23:14 +0100

    version1.2

--- Client/Client/Form1.Designer.cs
+++ Client/Client/Form1.Designer.cs
@@ -108,7 +108,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(139, 24);
             this.button4.TabIndex = 7;
-            this.button4.Text = "Alex";
+            this.button4.Text = "Porcentaje ganadas";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
